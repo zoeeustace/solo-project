5841bf79161782207f020fe3dccac08bcb4c36fb
Zoe Eustace <zoeeustace@Zoes-MacBook-Air.local>
2023-04-14 13:55:05 +0100

    second commit - added tables

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
+from controllers.cities_controller import cities_blueprint
+
+app = Flask(__name__)
+app.register_blueprint(cities_blueprint)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ db/travel_manager.sql
@@ -0,0 +1,15 @@
+DROP TABLE IF EXISTS cities;
+DROP TABLE IF EXISTS countries;
+
+CREATE TABLE countries (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255),
+  continent VARCHAR(255)
+);
+
+CREATE TABLE cities (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255),
+  visited BOOLEAN,
+  country_id INT NOT NULL REFERENCES countries(id)
+);
