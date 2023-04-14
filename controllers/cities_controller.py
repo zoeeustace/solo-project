8dcd8817651913c56fd7fc26a95c2c40739651dc
Zoe Eustace <zoeeustace@Zoes-MacBook-Air.local>
2023-04-14 16:02:18 +0100

    some routes added

---
+++ controllers/cities_controller.py
@@ -0,0 +1,30 @@
+from flask import Blueprint, render_template
+from repositories import city_repository, country_repository
+from models.city import City
+
+cities_blueprint = Blueprint("cities", __name__)
+
+#INDEX
+# GET /my-list
+@cities_blueprint.route("/my-list")
+def my_list():
+    all_cities = city_repository.select_all()
+    return render_template("cities/index.html", all_cities=all_cities)
+
+# NEW
+# GET /my-list/new
+
+# CREATE
+# POST /my-list
+
+# SHOW
+# GET /my-list/<id>
+
+# EDIT
+# GET /my-list/<id>/edit
+
+# UPDATE
+# PUT (POST) /my-list/<id>
+
+# DELETE
+# DELETE (POST) /my-list/<id>/delete
